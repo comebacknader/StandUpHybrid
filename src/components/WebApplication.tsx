18073f6cd905fda7fe9b958ede85ce18aeff797c
Nader Carun <comebacknader@gmail.com>
2020-10-26 00:21:57 -0700

    Added basic impressions screen

--- src/components/WebApplication.tsx
+++ src/components/WebApplication.tsx
@@ -1,17 +1,24 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { Image, StyleSheet, Text, View } from 'react-native';
-import { Layout } from '../styles';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Color, Layout } from '../styles';
+import Microphone from './Microphone';
 
 export default function WebApplication() {
   return (
     <View style={[Layout.flexAndDirectionRow]}>
       <View style={[styles.navigationBar]}>
-        <Image source={require('../assets/icon.png')} />
+        <Microphone />
       </View>
       <View style={[styles.mainContent]}>
-        <Text>Impressions</Text>
-        <StatusBar style="auto" />
+        <View style={[styles.impressionsContainer]}>
+          <View style={[styles.dot]} ></View>
+          <Text style={{fontSize: 22}}>Impressions</Text>
+        </View>
+        <View style={{flexDirection: 'row', justifyContent: 'center', alignItems: 'center', height: 500}}>
+          <Text style={{fontSize: 40, color: '#787878'}}>No impressions added.</Text>
+          <TouchableOpacity style={[styles.bigBlueDot, {marginLeft: 40}]} ><Text style={[styles.buttonText]}>+</Text></TouchableOpacity>
+        </View>
       </View>
     </View>
   )
@@ -19,10 +26,42 @@ export default function WebApplication() {
 
 const styles = StyleSheet.create({
   navigationBar: {
-    width: 100,
+    alignItems: 'center',
+    width: 90,
+    marginTop: 40,
   },
   mainContent: {
     ...Layout.isFlex, 
     backgroundColor: '#F2F2F2',
-  }
+  },
+  impressionsContainer: {
+    flexDirection: 'row',
+    marginTop: 40,
+    marginLeft: 50,
+  },
+  dot: {
+    width:5, 
+    height: 5, 
+    borderRadius: 50, 
+    borderColor: Color.SECONDARY_BLUE, 
+    borderWidth: 5, 
+    backgroundColor: Color.SECONDARY_BLUE, 
+    marginTop: 12, 
+    marginRight: 10 
+  },
+  bigBlueDot: {
+    width:50, 
+    height: 50, 
+    borderRadius: 50, 
+    borderColor: Color.SECONDARY_BLUE,  
+    backgroundColor: Color.SECONDARY_BLUE, 
+    marginTop: 12, 
+    marginRight: 10 
+  },
+  buttonText: {
+    alignSelf: 'center', 
+    color: 'white', 
+    fontSize: 30, 
+    fontWeight: 'bold'
+  },
 });
